161a5139497c891b0cf352cc326146a5e165733d
dstieby <dstieby@clemson.edu>
2017-04-20 22:40:53 -0400

    adding registration

--- app/src/main/java/com/runtigersrun/runtigers/Registration.java
+++ app/src/main/java/com/runtigersrun/runtigers/Registration.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registration extends AppCompatActivity {
 
@@ -21,5 +23,29 @@ public class Registration extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    public void clickRegister(View view){
+        EditText first = (EditText)findViewById(R.id.regEditFName);
+        EditText last = (EditText)findViewById(R.id.regEditLName);
+        EditText username = (EditText)findViewById(R.id.regEditUser);
+        EditText password = (EditText)findViewById(R.id.regEditPass);
+        EditText email = (EditText)findViewById(R.id.regEditEmail);
+
+        String fi = first.getText().toString();
+        String la = last.getText().toString();
+        String un = username.getText().toString();
+        String pass = password.getText().toString();
+        String em = email.getText().toString();
+
+        if(fi.isEmpty() || la.isEmpty() || un.isEmpty() || pass.isEmpty() || em.isEmpty()){
+            Toast.makeText(this, "All fields are required!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            User u = new User(fi,la,un,pass,em);
+            Toast.makeText(this, "User added!", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
