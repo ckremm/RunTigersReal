549a02357372c548aaa6c74e05d7b9016f98b796
cjkremm <ckremm@g.clemson.edu>
2017-04-20 22:07:44 -0400

    Expaneded User Class

--- app/src/main/java/com/runtigersrun/runtigers/User.java
+++ app/src/main/java/com/runtigersrun/runtigers/User.java
@@ -5,4 +5,21 @@ package com.runtigersrun.runtigers;
  */
 
 public class User {
+
+    private String FirstName;
+    private String LastName;
+    private String Username;
+    private String Password;
+    private String Email;
+
+
+    public User (String fname, String lname, String uname, String pass, String mail){
+        FirstName = fname;
+        LastName = lname;
+        Username = uname;
+        Password = pass;
+        Email = mail;
+
+    }
+
 }
