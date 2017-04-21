b1011f32524794a1aa875d3b4dab8c6523866793
cjkremm <ckremm@g.clemson.edu>
2017-04-20 21:45:44 -0400

    Test

--- app/src/main/java/com/runtigersrun/runtigers/MainActivity.java
+++ app/src/main/java/com/runtigersrun/runtigers/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Do you see this
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
