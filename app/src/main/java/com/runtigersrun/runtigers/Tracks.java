a9b08531797109b705021e8b1eff769590d1f620
cjkremm <ckremm@g.clemson.edu>
2017-04-23 13:31:01 -0400

    Adding Estimotes

--- app/src/main/java/com/runtigersrun/runtigers/Tracks.java
+++ app/src/main/java/com/runtigersrun/runtigers/Tracks.java
@@ -1,6 +1,7 @@
 package com.runtigersrun.runtigers;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +13,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class Tracks extends AppCompatActivity {
 
+    String JSON_STRING;
+    String j_string;
     String jdata;
     JSONObject jobj;
     JSONArray jarray;
@@ -76,5 +86,59 @@ public class Tracks extends AppCompatActivity {
                 Tracks.this.startActivity(userIntent);
             }
         });
+    }
+
+    public void toRoute(View view) {
+        Intent trackIntent = new Intent(Tracks.this, Route.class);
+        trackIntent.putExtra("Json_data", j_string);
+        Route.this.startActivity((trackIntent));
+    }
+
+    class backgroundtask extends AsyncTask<Void, Void, String> {
+        String json_url;
+        String json_urlUser;
+
+        @Override
+        protected void onPreExecute() {
+            json_url = " https://people.cs.clemson.edu/~dstieby/cpsc4820/RTR/externaldb/displaytracktext.php";
+            //json_urlUser = " https://people.cs.clemson.edu/~dstieby/cpsc4820/RTR/externaldb/displaytext.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(json_url);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                InputStream is = huc.getInputStream();
+                BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+                while ((JSON_STRING = bf.readLine()) != null) {
+                    sb.append(JSON_STRING + "\n");
+                }
+
+                bf.close();
+                is.close();
+                huc.disconnect();
+
+                return sb.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            j_string = result;
+        }
     }
 }
