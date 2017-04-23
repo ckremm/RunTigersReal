9b1c63170f69ebeb52e39c8e78499d41c7d75315
cjkremm <ckremm@g.clemson.edu>
2017-04-23 18:36:45 -0400

    Added Estimote Dependancies

--- app/src/main/java/com/runtigersrun/runtigers/activity/MainActivity.java
+++ app/src/main/java/com/runtigersrun/runtigers/activity/MainActivity.java
@@ -118,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
         for(User use : users){
             if(use.getUname().equals(un)){
                 if(use.getPass().equals(pa)){
-                    use = currentUser;
+                    currentUser = use;
                     Intent trackIntent = new Intent(MainActivity.this, Tracks.class);
                     trackIntent.putExtra("Json_data", j_string);
                     MainActivity.this.startActivity((trackIntent));
