1eb6b6dc42abd786e93e8e89e0fb42fbb78c2d36
aaronjv <aaronjv@g.clemson.edu>
2017-04-23 20:05:17 -0400

    leaderboards added

---
+++ app/src/main/java/com/runtigersrun/runtigers/Leaderboards.java
@@ -0,0 +1,13 @@
+package com.runtigersrun.runtigers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Leaderboards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboards);
+    }
+}
