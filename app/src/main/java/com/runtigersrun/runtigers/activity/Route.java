075ad3d4b074349e673813ffdb47be0d0244e5e7
aaronjv <aaronjv@g.clemson.edu>
2017-04-24 22:54:25 -0400

    Merge branch 'master' of C:\Users\aaronjv\AndroidStudioProjects\RunTigersReal with conflicts.

--- app/src/main/java/com/runtigersrun/runtigers/activity/Route.java
+++ app/src/main/java/com/runtigersrun/runtigers/activity/Route.java
@@ -82,7 +82,6 @@ public class Route extends AppCompatActivity {
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,7 +90,7 @@ public class Route extends AppCompatActivity {
         region = new Region("ranged region",
                 UUID.fromString("b9407f30-f5f8-466e-aff9-25556b57fe6d"), null, null);
 
-        Button b = (Button) findViewById(R.id.startButton);
+        Button b =(Button) findViewById(R.id.startButton);
 
         // Timer
         timerTextView = (TextView) findViewById(R.id.timerTextView);
@@ -163,18 +162,17 @@ public class Route extends AppCompatActivity {
     public void monitor(){
 
 
-        //Toast.makeText(this, "Inside monitor", Toast.LENGTH_LONG).show();
-        beaconManager = new BeaconManager(getApplicationContext());
-
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override
-            public void onServiceReady() {
-                beaconManager.startMonitoring(region);
-
-            }
-        });
-
         do {
+            //Toast.makeText(this, "Inside monitor", Toast.LENGTH_LONG).show();
+            beaconManager = new BeaconManager(getApplicationContext());
+
+            beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+                @Override
+                public void onServiceReady() {
+                    beaconManager.startMonitoring(region);
+
+                }
+            });
 
             beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
 
@@ -231,38 +229,55 @@ public class Route extends AppCompatActivity {
                             }
                         });
 
-                    } else if(val.equals(f.getMajor())){
-                        Toast.makeText(Route.this, "Found Ice" +
+                    }
+                else if(val.equals(f.getMajor())){
+                    Toast.makeText(Route.this, "Found Ice" +
                             "", Toast.LENGTH_LONG).show();
 
-                        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-                            @Override
-                            public void onInit(int status) {
-                                if (status == TextToSpeech.SUCCESS) {
-                                    int result = tts.setLanguage(Locale.US);
-                                    if (result==TextToSpeech.LANG_MISSING_DATA || result==TextToSpeech.LANG_NOT_SUPPORTED) {
-                                        Toast.makeText(getApplicationContext(), "Language not supported", Toast.LENGTH_LONG).show();
-                                    }
-                                    tts.speak("Found Ice",TextToSpeech.QUEUE_FLUSH, null);
-
+                    tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status == TextToSpeech.SUCCESS) {
+                                int result = tts.setLanguage(Locale.US);
+                                if (result==TextToSpeech.LANG_MISSING_DATA || result==TextToSpeech.LANG_NOT_SUPPORTED) {
+                                    Toast.makeText(getApplicationContext(), "Language not supported", Toast.LENGTH_LONG).show();
                                 }
-                            }
-                        });
+                                tts.speak("Found Ice",TextToSpeech.QUEUE_FLUSH, null);
 
-                    }
+                                timerHandler.removeCallbacks(timerRunnable);
+                                // uhhhhh something something put in database
+                            }
+                        }
+                    });
+
+                }
 
                 }
 
                 @Override
                 public void onExitedRegion(Region region) {
-                    Toast.makeText(Route.this, "here again", Toast.LENGTH_LONG).show();
+                    Toast.makeText(Route.this, "Exiting...", Toast.LENGTH_LONG).show();
+                    tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status == TextToSpeech.SUCCESS) {
+                                int result = tts.setLanguage(Locale.US);
+                                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                    Toast.makeText(getApplicationContext(), "Language not supported", Toast.LENGTH_LONG).show();
+                                }
+                                tts.speak("Exiting", TextToSpeech.QUEUE_FLUSH, null);
+
+                            }
+                        }
+                    });
                 }
             });
         }
+
         //PUT WHILE HERE
         while (isRunning);
 
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+        /*beaconManager.setRangingListener(new BeaconManager.RangingListener() {
             @Override
             public void onBeaconsDiscovered(Region region, List<Beacon> list) {
                 Toast.makeText(Route.this, String.valueOf(list.size()), Toast.LENGTH_SHORT).show();
@@ -289,8 +304,7 @@ public class Route extends AppCompatActivity {
                     }
                 }
             }
-        });
-
+        });*/
     }
 
     public void showNotification(String title, String message) {
